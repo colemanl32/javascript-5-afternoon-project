114e2ce421f502f270a33b8ce79bc71569ce21ce
Coleman Lindquist <coleman.lindquist.32@gmail.com>
2020-08-17 14:34:51 -0600

    Javascript 5 Afternoon project

--- classes.js
+++ classes.js
@@ -29,7 +29,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.makeWidget = function(){
+      return first_name + ' ' + last_name + ' Widget'
+   }
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +57,19 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee{
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age)
+    this.reports = []
+    
+  }
+  hire(employee) {
+    this.reports.push(employee)
+  }
+  fire(index) {
+    this.reports.splice(index, 1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -71,7 +93,34 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager{
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age)
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+  
+  hire(employee){
+    if(0 <= this.reports.length <= 2){
+      this.title = 'Barely Manager'
+    }
+    if(3 <= this.reports.length <= 9){
+      this.title = 'Mostly Manager'
+    }
+    if(10 <= this.reports.length <= 49){
+      this.title = 'Manager'
+    }
+    if(50 <= this.reports.length <= 99){
+      this.title = 'Manager Plus'
+    }
+    if(this.reports.length >= 100){
+      this.title = 'Bestest Manager'
+    }
+  }
+  fire(index){
+    this.bonus += 100
+  }
+}
 
 
 
